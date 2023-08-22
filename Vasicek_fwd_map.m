848765b98c58f10062877ed88fe933f00d7571ca
Ebenezer Nkum <63951159+enkum@users.noreply.github.com>
2023-08-21 21:36:48 -0600

    Add files via upload

---
+++ Vasicek_fwd_map.m
@@ -0,0 +1,24 @@
+% Copyright   : Michael Pokojovy & Valerii Maltsev (2020)
+% Version     : 2.0
+% Last edited : 11/27/2020
+% License     : Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+%               https://creativecommons.org/licenses/by-sa/4.0/
+
+function sol = Vasicek_fwd_map(T_grid, y0, alpha, r_ast, beta, n_rep)
+    dt = T_grid(2) - T_grid(1);
+
+    sol = zeros(length(T_grid), n_rep);
+    
+    a = alpha*dt;
+    b = alpha*r_ast*dt;
+    c = beta*sqrt(dt);
+    
+    for k = 1:n_rep 
+        sol(1, k) = y0;
+        for j = 2:length(T_grid)
+
+            sol(j, k) = b + (1 - a)*sol(j - 1, k) + c*randn(1);
+           
+        end
+    end
+end
---
+++ Vasicek_inv_map.m
@@ -0,0 +1,22 @@
+% Copyright   : Michael Pokojovy & Valerii Maltsev (2020)
+% Version     : 2.0
+% Last edited : 11/27/2020
+% License     : Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+%               https://creativecommons.org/licenses/by-sa/4.0/
+
+function [alpha_hat, r_ast_hat, beta_hat] = Vasicek_inv_map(T_grid, r_obs)
+    dt = T_grid(2) - T_grid(1);
+    
+    r_cur = r_obs(2:end);
+    r_lag = r_obs(1:(end - 1));
+    
+    m = length(T_grid) - 1;
+    
+    alpha_hat = (-1/dt)*log((m*sum(r_cur.*r_lag) - sum(r_cur)*sum(r_lag))/(m*sum(r_lag.^2) - (sum(r_lag))^2));
+    
+    expa  = exp(alpha_hat*dt);
+    expai = 1.0/expa;
+    r_ast_hat = 1/(m*(1 - expai))*(sum(r_cur) - expa*sum(r_lag));
+    
+    beta_hat = sqrt(2*alpha_hat/(m*(1 - expai^2))*sum((r_cur - r_lag*expai - r_ast_hat*(1 - expai)).^2));
+end
